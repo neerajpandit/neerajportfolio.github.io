997988a8f6f88a6a1cdc60a777715f09d686294a
neerajpandit <pandeyneeraj1507@gmail.com>
2023-07-14 09:28:26 +0530

    in

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Compuer Science Engineering Student", "Coder" , "Full Stack web Developer"],
+    strings: ["Compuer Science Engineering Student", "Coder" , "Backend web Developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
